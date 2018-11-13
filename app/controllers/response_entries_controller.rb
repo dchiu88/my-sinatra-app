634cb71bfe491fe714977719da26c7f576a48559
dchiu88 <davidwchiu@gmail.com>
2018-11-12 21:40:41 -0500

    created response entries controller

---
+++ app/controllers/response_entries_controller.rb
@@ -0,0 +1,14 @@
+class ResponseEntriesController < ApplicationController
+
+  #get response_entries/new to render a form to create new entry
+  get '/response_entries/new' do
+    erb :'/response_entries/new'
+  end
+  #post response_entries to create a new response entry
+
+  #show route for a response
+
+  #index route for all responses
+
+
+end
