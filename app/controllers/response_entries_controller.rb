cf13d1456ae6ed8e7ab835b47034af98a02b31ca
dchiu88 <davidwchiu@gmail.com>
2018-11-13 21:25:13 -0500

    add post entries, new form

--- app/controllers/response_entries_controller.rb
+++ app/controllers/response_entries_controller.rb
@@ -5,8 +5,21 @@ class ResponseEntriesController < ApplicationController
     erb :'/response_entries/new'
   end
   #post response_entries to create a new response entry
-
-  #show route for a response
+  post '/response_entries' do
+    #create a new entry and save it to the db
+    #only save if it has Content
+    #only create journal entry if user is logged in
+    if !logged_in?
+      redirect '/'
+    end
+    if params[:content] != ""
+      @response_entry = ResponseEntry.create(content: params[:content], user_id: current_user.id)
+      redirect "/response_entries/#{@response_entry.id}"
+    else
+      redirect '/response_entries/new'
+    end
+  end
+    #show route for a response
 
   #index route for all responses
 
